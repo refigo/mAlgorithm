6e538eadadc54f5e143c74ef071f3040459bb20f
Refi Go <64242999+refigo@users.noreply.github.com>
2021-09-04 14:45:55 +0900

    Update CodeUp C 1031~1037

--- practice_C/codeup/codeup1031_1037.c
+++ practice_C/codeup/codeup1031_1037.c
@@ -1,3 +1,100 @@
-# codeup c 1031~1037
+// codeup c 1031~1037
 
+//1031 : [기초-출력변환] 10진 정수 1개 입력받아 8진수로 출력하기(설명)
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	num;
+
+	scanf("%d", &num);
+	printf("%o\n", num);
+	return (0);
+}
+*/
+
+//1032 : [기초-출력변환] 10진 정수 입력받아 16진수로 출력하기1(설명)
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	num;
+
+	scanf("%d", &num);
+	printf("%x\n", num);
+	return (0);
+}
+*/
+
+//1033 : [기초-출력변환] 10진 정수 입력받아 16진수로 출력하기2(설명)
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	num;
+
+	scanf("%d", &num);
+	printf("%X\n", num);
+	return (0);
+}
+*/
+
+//1034 : [기초-출력변환] 8진 정수 1개 입력받아 10진수로 출력하기(설명)
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	num;
+
+	scanf("%o", &num);
+	printf("%d\n", num);
+	return (0);
+}
+*/
+
+//1035 : [기초-출력변환] 16진 정수 1개 입력받아 8진수로 출력하기(설명)
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	num;
+
+	scanf("%x", &num);
+	printf("%o\n", num);
+	return (0);
+}
+*/
+
+//1036 : [기초-출력변환] 영문자 1개 입력받아 10진수로 출력하기(설명)
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char	c;
+
+	scanf("%c", &c);
+	printf("%d\n", c);
+	return (0);
+}
+*/
+
+//1037 : [기초-출력변환] 정수 입력받아 아스키 문자로 출력하기
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	ascii_num;
+
+	scanf("%d", &ascii_num);
+	printf("%c\n", ascii_num);
+	return (0);
+}
+*/
 
