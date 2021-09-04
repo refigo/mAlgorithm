324cbe3a695e6d2b2dc1ce4c7853469d00a632f4
Refi Go <64242999+refigo@users.noreply.github.com>
2021-09-04 09:57:40 +0900

    Update codeup C 1001~1008

--- practice_C/codeup/codeup1001_1008.c
+++ practice_C/codeup/codeup1001_1008.c
@@ -1,3 +1,92 @@
-# codeup c 1001~1008
+// codeup c 1001~1008
 
+//1001 : [기초-출력] 출력하기01(설명)
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("Hello");
+	return (0);
+}
+*/
+
+//1002 : [기초-출력] 출력하기02(설명)
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("Hello World");
+	return (0);
+}
+*/
+
+//1003 : [기초-출력] 출력하기03(설명)
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("Hello\nWorld");
+	return (0);
+}
+*/
+
+//1004 : [기초-출력] 출력하기04(설명)
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("\'Hello\'");
+	return (0);
+}
+*/
+
+//1005 : [기초-출력] 출력하기05(설명)
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("\"Hello World\"");
+	return (0);
+}
+*/
+
+//1006 : [기초-출력] 출력하기06(설명)⭐️
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("\"!@#$%%^&*()\"");
+	return (0);
+}
+*/
+
+//1007 : [기초-출력] 출력하기07(설명)
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("\"C:\\Download\\hello.cpp\"");
+	return (0);
+}
+*/
+
+//1008 : [기초-출력] 출력하기08(설명)⭐️
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("\u250C\u252C\u2510\n");
+	printf("\u251C\u253C\u2524\n");
+	printf("\u2514\u2534\u2518\n");
+	return (0);
+}
+*/
 
